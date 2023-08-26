0bd8fb171edadcdbff5c8db07b5d6a5e7a6d4a8d
Mehrshad Cheshm Khavari <ytmehrshad@gmail.com>
2023-08-26 17:34:11 +0330

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Selecting needed elements
+const box = document.querySelector(".drawing-box");
+
+// Creating the dot element
+
+// Functions
+const draw = function (e) {
+  // Selecting the touch
+  const rect = e.target.closest(".drawing-box").getBoundingClientRect();
+  const [touch] = [...e.changedTouches];
+
+  // Creating the dot element
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+
+  // Setting the position of the dot
+  dot.style.left = touch.clientX - rect.left + "px";
+  dot.style.top = touch.clientY - rect.top + "px";
+
+  box.insertAdjacentElement("beforeend", dot);
+};
+
+// Event listeners
+box.addEventListener("touchstart", draw);
+box.addEventListener("touchmove", draw);
